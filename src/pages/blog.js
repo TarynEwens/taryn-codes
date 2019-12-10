8868f89be6abc98eb9d62d9e669f0e33f566cc7b
Taryn <taryn@taryn.codes>
2019-12-10 11:31:18 +1100

    feat(blog): add link to new external post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -46,6 +46,7 @@ const BlogIndex = (props) => {
       <div>
         <h2>My writing on other sites</h2>
         <ul className="blog__externalList">
+          <li><a href="https://medium.com/finder-tech/imposter-syndrome-all-the-way-up-9d90753ad38c" rel="noopener noreferrer" target="_blank">Imposter syndrome all the way up</a></li>
           <li><a href="https://medium.com/finder-tech/analysing-our-engineering-teams-key-values-e9f79dd4391e" rel="noopener noreferrer" target="_blank">Analysing our engineering teams key values</a></li>
         </ul>
       </div>
