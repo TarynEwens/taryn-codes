633cab0a40a892a76a278f29ff8d3902f547aae7
Taryn <taryn@taryn.codes>
2019-12-11 14:14:25 +1100

    feat(quotes): add quotes adjust style

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -34,6 +34,28 @@ const Quotes = (props) => {
       "Me: Why is my JavaScript not working? Also me: using PHP syntax",
 
       "My jQuery is so complex it disgusts me.",
+
+      "I love a good implode()",
+
+      "Full stack yo!",
+
+      "If I were a hacker I'd fill horrible peoples websites with clown emojis.",
+
+      "console.log('is this where the error is?');",
+
+      "Me this morning: I'm going to stop work at 4.30 and smash out a couple of Javascript30's. Me now: error error error error error error error error",
+
+      "I just described my outfit as metro lumberjack vibes with a touch of Williams Wish Wellingtons",
+
+      "It's like the dev gods were like 'just cause you're an engineer now, don't get a big head - the first deployment you do will break production!'",
+
+      "I ate so many dumplings for lunch I feel like I am a dumpling now.",
+
+      "Me: 'It's not time yet I'm still working'. Lexi: 'arooorooooooo'. Me: 'No Lexi, be patient'. Lexi: 'arrooo'.",
+
+      "If TypeScript was an object I'd have thrown it in the bin by now",
+
+      "Ejecting create react app isn't quite like painting a landscape...",
     ]
 
     let n = Math.floor(Math.random() * quotes.length);
