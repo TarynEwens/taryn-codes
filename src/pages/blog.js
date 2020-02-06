241ac5449fd9ba2e30aeefa8210f751e60ce50e6
Taryn <taryn@taryn.codes>
2020-02-06 12:37:49 +1100

    feat(blog): add link to alt paths article

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -46,8 +46,9 @@ const BlogIndex = (props) => {
       <div>
         <h2>My writing on other sites</h2>
         <ul className="blog__externalList">
-          <li><a href="https://medium.com/finder-tech/imposter-syndrome-all-the-way-up-9d90753ad38c" rel="noopener noreferrer" target="_blank">Imposter syndrome all the way up</a></li>
-          <li><a href="https://medium.com/finder-tech/analysing-our-engineering-teams-key-values-e9f79dd4391e" rel="noopener noreferrer" target="_blank">Analysing our engineering teams key values</a></li>
+          <li><a href="https://www.projectf.com.au/blog/2020/1/30/alternative-paths-to-software-engineering-for-women">Alternative paths to software engineering for women</a></li>
+          <li><a href="https://medium.com/finder-tech/imposter-syndrome-all-the-way-up-9d90753ad38c">Imposter syndrome all the way up</a></li>
+          <li><a href="https://medium.com/finder-tech/analysing-our-engineering-teams-key-values-e9f79dd4391e">Analysing our engineering teams key values</a></li>
         </ul>
       </div>
     </Layout>
